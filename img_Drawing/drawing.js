9233cff35954a9b9fb7826e0549c77b77503c2ed
laiwenbang <375500819@qq.com>
2016-03-01 19:36:35 +0800

    解决快速移动出现的问题， 让按钮停留300毫秒后再执行图片切换

--- img_Drawing/drawing.js
+++ img_Drawing/drawing.js
@@ -9,84 +9,91 @@
         });
  */
 
-!(function($){
+ !(function($,window,document,undefined){
 
-        //构造函数
-        var imgs = function(opt){
-            this.imgId = $(opt.imgArea);
-            this.btns = $(opt.listBtn);
-            this.time = opt.time;  //每多少秒走一次
-            this.btnLis = this.btns.find('li');
-            this.imgLis = this.imgId.find('li');
-            this.cindex = 0;
-            this.maxIndex = this.imgLis.length-1;
-            this.flag; //计时器
-        }
+         //构造函数
+         var imgs = function(opt){
+             this.imgId = $(opt.imgArea);
+             this.btns = $(opt.listBtn);
+             this.time = opt.time;  //每多少秒走一次
+             this.btnLis = this.btns.find('li');
+             this.imgLis = this.imgId.find('li');
+             this.cindex = 0;
+             this.maxIndex = this.imgLis.length-1;
+             this.flag; //计时器
+         }
 
-        //给按钮绑定事件
-        imgs.prototype.bindEvent = function(){
+         //给按钮绑定事件
+         imgs.prototype.bindEvent = function(){
 
-            this.eventHandle(this.btnLis);
-            this.eventHandle(this.imgLis);
-        }
+             this.eventHandle(this.btnLis);
+             this.eventHandle(this.imgLis);
+         }
 
-        //事件
-        imgs.prototype.eventHandle = function(ele)
-        {
-            var that = this;
-            ele.mouseenter(function() {
-                //判断是否存在自动播放标记
-                clearInterval(that.flag);
-                var index = $(this).index();
-                setTimeout(function(){
-                    that.switchImg(index)
-                },200);
-            }).mouseleave(function(){
-                var index = $(this).index();
-                that.autoRun();
-            });
-        }
+         //事件
+         imgs.prototype.eventHandle = function(ele)
+         {
+             var that = this;
+             var timeId = 0;
+             ele.mouseover(function() {
+                 var isThat = $(this);
+                 //判断是否存在自动播放标记
+                 clearTimeout(timeId);
+                 timeId = setTimeout(function(){
+                     clearInterval(that.flag);
+                     var index = isThat.index();
+                     that.switchImg(index)
+                 },350)
 
-        //切换图片
-        imgs.prototype.switchImg =  function(menuIndex)
-        {
-            //当前走到第几张图片
-            this.cindex = menuIndex;
-            var cImg  = this.imgLis.eq(menuIndex);
-            var cMenu = this.btnLis.eq(menuIndex);
-            cMenu.addClass('active').siblings().removeClass('active');
-            cImg.stop().fadeIn('fast').siblings().hide();
-        }
+             }).mouseout(function(){
+                 clearTimeout(timeId);
+                 var index = $(this).index();
+                 that.autoRun();
+             });
+         }
 
-        //自动播放
-        imgs.prototype.autoRun = function(){
-            var that = this;
-            this.flag = setInterval(function(){
-                that.cindex++;
-                if(that.cindex>that.maxIndex)
-                {
-                    that.cindex = 0;
-                }
-                that.switchImg(that.cindex);
+         //切换图片
+         imgs.prototype.switchImg =  function(menuIndex)
+         {
+             //当前走到第几张图片
+             this.cindex = menuIndex;
+             var cImg  = this.imgLis.eq(menuIndex);
+             var cMenu = this.btnLis.eq(menuIndex);
+             cMenu.addClass('active').siblings().removeClass('active');
+             cImg.stop().fadeIn('fast').siblings().fadeOut();
+             console.log(this.cindex);
+         }
 
-            },that.time);
-        }
+         //自动播放
+         imgs.prototype.autoRun = function(){
+             var that = this;
+             clearInterval(this.flag);
+             this.flag = setInterval(function(){
+                 that.cindex++;
+                 if(that.cindex>that.maxIndex)
+                 {
+                     that.cindex = 0;
+                 }
+                 that.switchImg(that.cindex);
 
-        //初始化方法
-        imgs.prototype.run = function(){
-            this.bindEvent();
-            this.autoRun();
-        }
+             },that.time);
+         }
 
-        $.fn.imgRuning = function(opt){
-            var dopt = {
-                'imgArea':opt.imgArea,
-                'time':opt.time,
-                'listBtn':$(this)
-            }
-            var igs = new imgs(dopt);
-            igs.run();
-            return this;
-        }
+         //初始化方法
+         imgs.prototype.run = function(){
+             this.bindEvent();
+             this.autoRun();
+         }
 
-    })(jQuery);
+         $.fn.imgRuning = function(opt){
+             var dopt = {
+                 'imgArea':opt.imgArea,
+                 'time':opt.time,
+                 'listBtn':$(this)
+             }
+             var igs = new imgs(dopt);
+             igs.run();
+             return this;
+         }
+
+     })(jQuery,window,document);
